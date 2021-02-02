c86487ae1fb51e48ef761f506c4f9dcafef62f29
slumder <war7951@gmail.com>
2021-02-02 10:55:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+			<h1>COVID-19 Tracking Dashboard</h1>
+			<LineChart />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import LineChart from '@/components/LineChart'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+		LineChart,
+	},
+
+  data: () => ({
+    //
+  }),
+
+	async created() {
+		let {data} = await axios.get('https://api.covidtracking.com/v1/us/daily.json')
+		console.log(data)
+	},
+};
+</script>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  mounted() {
+    const dates = [
+      "2021/01/01",
+      "2021/01/02",
+      "2021/01/03",
+      "2021/01/04",
+      "2021/01/05",
+      "2021/01/06",
+    ];
+    const totals = [50, 40, 71, 51, 25, 49];
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: "demo",
+            data: totals,
+          },
+        ],
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+      }
+    );
+  },
+};
+</script>
