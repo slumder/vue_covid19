d163f072971548f0072dd11dbe828a2741602fe2
slumder <war7951@gmail.com>
2021-02-02 11:55:07 +0800

    finish project

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,33 +1,53 @@
 <script>
-import { Line } from "vue-chartjs";
+import {Line} from "vue-chartjs";
 
 export default {
   extends: Line,
+
+  // 接收 父參數
+  props: {
+    label: {
+      type: String,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+    chartColorOptions: {
+      type: Object,
+    },
+  },
   mounted() {
-    const dates = [
-      "2021/01/01",
-      "2021/01/02",
-      "2021/01/03",
-      "2021/01/04",
-      "2021/01/05",
-      "2021/01/06",
-    ];
-    const totals = [50, 40, 71, 51, 25, 49];
+    // const dates = [
+    //   "2021/01/01",
+    //   "2021/01/02",
+    //   "2021/01/03",
+    //   "2021/01/04",
+    //   "2021/01/05",
+    //   "2021/01/06",
+    // ];
+    // const totals = [50, 40, 71, 51, 25, 49];
+    const dates = this.chartData.map((d) => d.date).reverse();
+    const totals = this.chartData.map((d) => d.total).reverse();
 
     this.renderChart(
       {
         labels: dates,
         datasets: [
           {
-            label: "demo",
+            label: this.label,
             data: totals,
+            ...this.chartColorOptions,
           },
         ],
       },
-      {
-        responsive: true,
-        maintainAspectRatio: false,
-      }
+      // {
+      //   responsive: true,
+      //   maintainAspectRatio: false,
+      // }
+      this.options
     );
   },
 };
